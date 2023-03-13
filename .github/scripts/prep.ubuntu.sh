6363fe4ce81815ecec930ce9263ff4056a1b1846
a.fernandez3 <a.fernandez3@udc.es>
2023-03-13 13:18:12 +0100

    updated mpi

--- .github/scripts/prep.ubuntu.sh
+++ .github/scripts/prep.ubuntu.sh
@@ -4,15 +4,7 @@
 sudo apt-get -o Acquire::ForceIPv4=true update -qq 1>/dev/null
 sudo apt-get -o Acquire::ForceIPv4=true upgrade -qq 1>/dev/null
 sudo apt-get -o Acquire::ForceIPv4=true install \
-    gcc-10 g++-10 cmake libomp-dev -qq 1>/dev/null
-
-# Send std out to null
-wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.5.tar.bz2 -O openmpi.tar.bz2 1>/dev/null 2>&1
-tar -xjvf openmpi.tar.bz2 1>/dev/null 2>&1
-cd openmpi-4.1.5
-mkdir build && cd build
-../configure
-make all install
+    gcc-10 g++-10 cmake libomp-dev openmpi-bin libopenmpi4 libopenmpi-dev -qq 1>/dev/null
 
 # Fix openmpi complaining about not having enough slots
 OPENMPI_HOSTFILE=/etc/openmpi/openmpi-default-hostfile
@@ -22,7 +14,6 @@ if [ -z "$(cat ${OPENMPI_HOSTFILE} | grep -v -E '^#')" ]; then\
 fi
 
 # Install HTSlib
-cd
 wget https://github.com/samtools/htslib/releases/download/1.17/htslib-1.17.tar.bz2 -O htslib.tar.bz2 1>/dev/null
 tar -xjvf htslib.tar.bz2 1>/dev/null
 cd htslib-1.17 

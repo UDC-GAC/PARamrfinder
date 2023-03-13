2efcd1a4297f552ca942dea0ec8dba7f41037eb4
a.fernandez3 <a.fernandez3@udc.es>
2023-03-13 11:11:04 +0100

    added HTSlib to dependencies

--- .github/scripts/prep.ubuntu.sh
+++ .github/scripts/prep.ubuntu.sh
@@ -3,7 +3,7 @@
 # Install dependencies
 sudo apt-get -o Acquire::ForceIPv4=true update -qq 1>/dev/null
 sudo apt-get -o Acquire::ForceIPv4=true upgrade -qq 1>/dev/null
-sudo apt-get -o Acquire::ForceIPv4=true install \
+sudo apt-get -o Acquire::ForceIPv4=true install -Y \
     gcc-10 g++-10 cmake libomp-dev openmpi-bin libopenmpi3 libopenmpi-dev -qq 1>/dev/null
 
 # Fix openmpi complaining about not having enough slots
@@ -12,3 +12,11 @@ OPENMPI_HOSTFILE=/etc/openmpi/openmpi-default-hostfile
 if [ -z "$(cat ${OPENMPI_HOSTFILE} | grep -v -E '^#')" ]; then\
     echo \"localhost slots=32\" | sudo tee -a ${OPENMPI_HOSTFILE};\
 fi
+
+# Install HTSlib
+wget https://github.com/samtools/htslib/releases/download/1.17/htslib-1.17.tar.bz2 -O htslib.tar.bz2
+tar -xjvf htslib.tar.bz2
+cd htslib-1.17
+./configure
+make
+sudo make install

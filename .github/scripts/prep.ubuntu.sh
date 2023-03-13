69f34e3282a00d349315ad7b6816522aca85e7e7
a.fernandez3 <a.fernandez3@udc.es>
2023-03-13 10:57:06 +0100

    update workflow

--- .github/scripts/prep.ubuntu.sh
+++ .github/scripts/prep.ubuntu.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-sudo apt-get update -qq 1>/dev/null
-sudo apt-get upgrade -qq 1>/dev/null
-sudo apt-get install -Y gcc-10 g++-10 cmake libomp-dev openmpi-bin libopenmpi3 libopenmpi-dev -qq 1>/dev/null
+sudo apt-get -o Acquire::ForceIPv4=true update -qq 1>/dev/null
+sudo apt-get -o Acquire::ForceIPv4=true upgrade -qq 1>/dev/null
+sudo apt-get -o Acquire::ForceIPv4=true install \
+    gcc-10 g++-10 cmake libomp-dev openmpi-bin libopenmpi3 libopenmpi-dev -qq 1>/dev/null
 
 # Fix openmpi complaining about not having enough slots
 OPENMPI_HOSTFILE=/etc/openmpi/openmpi-default-hostfile
